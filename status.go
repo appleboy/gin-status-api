6e9d17f8a41d142221548f0493486dfffc492652
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-04-20 21:31:45 +0800

    update http code

--- status.go
+++ status.go
@@ -3,9 +3,10 @@ package status
 import (
 	api "github.com/fukata/golang-stats-api-handler"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // StatusHandler is gin handle for get system status.
 func StatusHandler(c *gin.Context) {
-	c.JSON(200, api.GetStats())
+	c.JSON(http.StatusOK, api.GetStats())
 }
